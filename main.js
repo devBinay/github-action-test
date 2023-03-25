a18c3aac251edcb0b0d4b825bec985aa7f048cb2
devBinay <54987551+devBinay@users.noreply.github.com>
2023-03-25 11:30:32 +0000

    Deploying to gh-pages from @ devBinay/github-action-test@9042935928d824b40659516b4282557f15f601a6 🚀

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const postTemplate = (title, desc, imgSrc) => `<div>
 <p>${title}</p>
-<span>${desc}<span>
+<span>${desc}</span>
 <img src="${imgSrc}">
 </div>`
 
@@ -10,5 +10,5 @@ window.fetch('./data.json').then(res => res.json()).then(res => {
   posts = posts.map(p => {
     return postTemplate(p.title, p.description, p.imgSrc)
   })
-  app.innerHTML = posts.join('/n')
+  app.innerHTML = posts.join('\n')
 })
